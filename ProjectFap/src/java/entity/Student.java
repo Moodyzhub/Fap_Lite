861f8e508e99c57d5e9795164a454c2a532579ea
Moodyzhub <phatuzumaki@gmail.com>
2024-03-12 09:56:11 +0700

    student for attendence

---
+++ ProjectFap/src/java/entity/Student.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Fatvv
+ */
+class Student {
+    private int std_id;
+    private String std_name;
+
+    public int getStd_id() {
+        return std_id;
+    }
+
+    public void setStd_id(int std_id) {
+        this.std_id = std_id;
+    }
+
+    public String getStd_name() {
+        return std_name;
+    }
+
+    public void setStd_name(String std_name) {
+        this.std_name = std_name;
+    }
+    
+}
