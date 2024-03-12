7d4fbb4713338dcc7ceb934f024f68e804f89586
Moodyzhub <phatuzumaki@gmail.com>
2024-03-12 09:31:49 +0700

    Create TimeSlot entity
    gồm id =1 và name= slot1

---
+++ ProjectFap/src/java/entity/TimeSlot.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Fatvv
+ */
+public class TimeSlot {
+    private int time_id;
+    private int time_name;
+
+    public int getTime_id() {
+        return time_id;
+    }
+
+    public void setTime_id(int time_id) {
+        this.time_id = time_id;
+    }
+
+    public int getTime_name() {
+        return time_name;
+    }
+
+    public void setTime_name(int time_name) {
+        this.time_name = time_name;
+    }
+    
+    
+}
