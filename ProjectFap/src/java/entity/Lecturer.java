165aa48c57593fec0aad7816ac20506c7b3b77fb
Moodyzhub <phatuzumaki@gmail.com>
2024-03-12 09:47:51 +0700

    Lecture for Timetable

---
+++ ProjectFap/src/java/entity/Lecturer.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Fatvv
+ */
+class Lecturer {
+    private int lec_id;
+    private String lec_name;
+
+    public int getLec_id() {
+        return lec_id;
+    }
+
+    public void setLec_id(int lec_id) {
+        this.lec_id = lec_id;
+    }
+
+    public String getLec_name() {
+        return lec_name;
+    }
+
+    public void setLec_name(String lec_name) {
+        this.lec_name = lec_name;
+    }
+    
+    
+}
