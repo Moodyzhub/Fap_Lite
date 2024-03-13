babb08f81b3f4a4635055aaee8619dc9a8129078
Moodyzhub <phatuzumaki@gmail.com>
2024-03-14 00:04:57 +0700

    clear timetablecontroller
    xóa hết các phần import từ phần của thầy sơn

--- ProjectFap/src/java/controller/lecturer/TimeTableController.java
+++ ProjectFap/src/java/controller/lecturer/TimeTableController.java
@@ -1,30 +1,10 @@
 package controller.lecturer;
 
-import controller.authentication.BaseRequiredAuthenticationController;
-import controller.authentication.authorization.BaseRBACController;
-import entity.Account;
-import entity.Lession;
-import entity.Role;
-import entity.TimeSlot;
-import java.io.IOException;
-import java.io.PrintWriter;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Date;
+
 
 
 
 public class TimeTableController extends BaseRBACController {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account,ArrayList<Role> roles) throws ServletException, IOException {
-    
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account,ArrayList<Role> roles) throws ServletException, IOException {
-}
+   
 }
