b45be71e98713ddeed8b16a08a2120930b7d3e0b
Moodyzhub <phatuzumaki@gmail.com>
2024-03-13 13:37:44 +0700

    demo first time timetablecontroller with pagination

--- ProjectFap/src/java/controller/lecturer/TimeTableController.java
+++ ProjectFap/src/java/controller/lecturer/TimeTableController.java
@@ -1,34 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+package controller.assignment.lecturer;
 
-package controller.lecturer;
-
+import controller.authentication.BaseRequiredAuthenticationController;
 import controller.authentication.authorization.BaseRBACController;
+import dal.assignment.LessionDBContext;
+import dal.assignment.TimeSlotDBContext;
 import entity.Account;
-import entity.Role;
-import java.io.IOException;
-import java.io.PrintWriter;
+import entity.assignment.Lession;
+import entity.assignment.Role;
+import entity.assignment.TimeSlot;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import util.DateTimeHelper;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
-/**
- *
- * @author Fatvv
- */
 public class TimeTableController extends BaseRBACController {
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String raw_from = req.getParameter("from");
+        String raw_to = req.getParameter("to");
+        String raw_lid = req.getParameter("lid");
         
+        String pageParam = req.getParameter("page");
+        int page;
+        if (pageParam == null || !pageParam.matches("\\d+")) {
+            page = 1;
+        } else {
+            page = Integer.parseInt(pageParam);
+        }
+        
+        java.sql.Date from;
+        java.sql.Date to;
+        int lid = Integer.parseInt(raw_lid);
+        
+        Date today = new Date();
+        if(raw_from == null)
+        {
+            from = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.getWeekStart(today));
+        }
+        else
+        {
+            from = java.sql.Date.valueOf(raw_from);
+        }
+        
+        if(raw_to == null)
+        {
+            to = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.
+                    addDaysToDate(DateTimeHelper.getWeekStart(today),6));
+        }
+        else
+        {
+            to = java.sql.Date.valueOf(raw_to);
+        }
+        
+        ArrayList<java.sql.Date> dates = DateTimeHelper.getDatesBetween(from, to, page);
+        
+        TimeSlotDBContext slotDB = new TimeSlotDBContext();
+        ArrayList<TimeSlot> slots = slotDB.list();
+        
+        LessionDBContext lessDB = new LessionDBContext();
+        ArrayList<Lession> lessions = lessDB.getBy(lid, from, to);
+        
+        req.setAttribute("from", from);
+        req.setAttribute("to", to);
+        req.setAttribute("slots", slots);
+        req.setAttribute("dates", dates);
+        req.setAttribute("lessions", lessions);
+        req.setAttribute("page", page);
+        req.getRequestDispatcher("../view/lecturer/timetable.jsp").forward(req, resp);
     }
-
 }
