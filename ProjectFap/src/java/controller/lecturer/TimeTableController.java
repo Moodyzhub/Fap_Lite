37c57dfe545b3cc7add1673c91f2dc36a0ba06ef
Moodyzhub <phatuzumaki@gmail.com>
2024-03-13 22:20:47 +0700

    delete teacher's code

--- ProjectFap/src/java/controller/lecturer/TimeTableController.java
+++ ProjectFap/src/java/controller/lecturer/TimeTableController.java
@@ -1,13 +1,11 @@
-package controller.assignment.lecturer;
+package controller.lecturer;
 
 import controller.authentication.BaseRequiredAuthenticationController;
 import controller.authentication.authorization.BaseRBACController;
-import dal.assignment.LessionDBContext;
-import dal.assignment.TimeSlotDBContext;
 import entity.Account;
-import entity.assignment.Lession;
-import entity.assignment.Role;
-import entity.assignment.TimeSlot;
+import entity.Lession;
+import entity.Role;
+import entity.TimeSlot;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -16,7 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Date;
-import util.DateTimeHelper;
+
 
 
 public class TimeTableController extends BaseRBACController {
@@ -28,49 +26,5 @@ public class TimeTableController extends BaseRBACController {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account,ArrayList<Role> roles) throws ServletException, IOException {
-        int lid = Integer.parseInt(req.getParameter("id"));
-        String raw_from = req.getParameter("from");
-        String raw_to = req.getParameter("to");
-        Date today = new Date();
-        java.sql.Date from = null; 
-        java.sql.Date to = null;
-        
-        if(raw_from ==null)
-        {
-            from = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.getWeekStart(today));
-        }
-        else
-        {
-            from = java.sql.Date.valueOf(raw_from);
-        }
-        
-        if(raw_to == null)
-        {
-            to = DateTimeHelper.convertUtilDateToSqlDate(DateTimeHelper.
-                    addDaysToDate(DateTimeHelper.getWeekStart(today),6));
-        }
-        else
-        {
-            to = java.sql.Date.valueOf(raw_to);
-        }
-        
-        ArrayList<java.sql.Date> dates = DateTimeHelper.getDatesBetween(from, to);
-        
-        TimeSlotDBContext slotDB = new TimeSlotDBContext();
-        ArrayList<TimeSlot> slots = slotDB.list();
-        
-        LessionDBContext lessDB = new LessionDBContext();
-        ArrayList<Lession> lessions = lessDB.getBy(lid, from, to);
-        
-        req.setAttribute("from", from);
-        req.setAttribute("to", to);
-        req.setAttribute("slots", slots);
-        req.setAttribute("dates", dates);
-        req.setAttribute("lessions", lessions);
-        req.getRequestDispatcher("../view/lecturer/timetable.jsp").forward(req, resp);
-        
-    }
-   
-   
-
+}
 }
