1b3c6e1dc19273044015b387189ec05f5788bd7b
Moodyzhub <phatuzumaki@gmail.com>
2024-03-14 09:55:53 +0700

    create getStudentLesson fuction
    để lấy đc data về lesson khi biết id của student

--- ProjectFap/src/java/dal/LessonDBContext.java
+++ ProjectFap/src/java/dal/LessonDBContext.java
@@ -20,7 +20,8 @@ import java.util.logging.Logger;
  *
  * @author Fatvv
  */
-public class LessonDBContext extends DBContext<Lesson>{
+public class LessonDBContext extends DBContext<Lesson> {
+
     public ArrayList<Lesson> getLecturerLesson(int lecid, Date from, Date to) {
         ArrayList<Lesson> lessons = new ArrayList<>();
         try {
@@ -76,16 +77,78 @@ public class LessonDBContext extends DBContext<Lesson>{
                 lessons.add(les);
             }
 
-        }  catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(LessonDBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
         return lessons;
     }
-    
-    
-    
-    
-    
+
+    public ArrayList<Lesson> getStudentLesson(int stuid, Date from, Date to) {
+        ArrayList<Lesson> lessons = new ArrayList<>();
+        try {
+            String sql = "SELECT \n"
+                    + "les.leid, les.isAttended, les.date, \n"
+                    + "s.sid, s.sname, \n"
+                    + "g.gid, g.gname, su.subid, su.suname, \n"
+                    + "t.tid, t.tname, \n"
+                    + "r.rid, r.rname, \n"
+                    + "l.lid, l.lname \n"
+                    + "FROM \n"
+                    + "Lession les \n"
+                    + "INNER JOIN StudentGroup g ON g.gid = les.gid \n"
+                    + "INNER JOIN Subject su ON su.subid = g.subid \n"
+                    + "INNER JOIN TimeSlot t ON t.tid = les.tid \n"
+                    + "INNER JOIN Room r ON r.rid = les.rid \n"
+                    + "INNER JOIN Lecturer l ON l.lid = les.lid \n"
+                    + "INNER JOIN Enrollment e ON e.gid = g.gid \n"
+                    + "INNER JOIN Student s ON s.sid = e.sid \n"
+                    + "WHERE s.sid=? AND les.[date] >=? AND les.[date]<=?";
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setInt(1, stuid);
+            stm.setDate(2, from);
+            stm.setDate(3, to);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Lesson les = new Lesson();
+                StudentGroup sg = new StudentGroup();
+                Subject sub = new Subject();
+                TimeSlot slot = new TimeSlot();
+                Room room = new Room();
+                Lecturer lec = new Lecturer();
+
+                les.setId(rs.getInt("leid"));
+                les.setAttended(rs.getBoolean("isAttended"));
+                les.setDate(rs.getDate("date"));
+
+                sg.setStdg_id(rs.getInt("gid"));
+                sg.setStdg_name(rs.getString("gname"));
+                sub.setSub_id(rs.getInt("subid"));
+                sub.setSub_name(rs.getString("suname"));
+                sg.setSubject(sub);
+
+                les.setGroup(sg);
+
+                slot.setTime_id(rs.getInt("tid"));
+                slot.setTime_name(rs.getString("tname"));
+                les.setSlot(slot);
+
+                room.setRoom_id(rs.getInt("rid"));
+                room.setRoom_name(rs.getString("rname"));
+                les.setRoom(room);
+
+                lec.setLec_id(rs.getInt("lid"));
+                lec.setLec_name(rs.getString("lname"));
+                les.setLecturer(lec);
+
+                lessons.add(les);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LessonDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return lessons;
+    }
 
     @Override
     public ArrayList<Lesson> list() {
@@ -111,5 +174,5 @@ public class LessonDBContext extends DBContext<Lesson>{
     public Lesson get(int id) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
