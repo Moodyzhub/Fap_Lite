92ee7ec4a297b0499a832db6dd42e4a6bc3663ed
Moodyzhub <phatuzumaki@gmail.com>
2024-03-14 23:11:14 +0700

    get parameter and init needed objects
    lấy year và day từ jsp để xử lí phần chọn thời gian

--- ProjectFap/src/java/controller/lecturer/TimeTableLecturerController.java
+++ ProjectFap/src/java/controller/lecturer/TimeTableLecturerController.java
@@ -5,18 +5,27 @@
 
 package controller.lecturer;
 
+import controller.authentication.authorization.BaseRBACController;
+import dal.LessonDBContext;
+import entity.Account;
+import entity.Lesson;
+import entity.Role;
+import entity.Week;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
  * @author Fatvv
  */
-public class TimeTableLecturerController extends HttpServlet {
+public class TimeTableLecturerController extends BaseRBACController {
    
     /** 
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
@@ -25,57 +34,33 @@ public class TimeTableLecturerController extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TimeTableLecturerController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet TimeTableLecturerController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String raw_year = req.getParameter("year");
+        String raw_startDay = req.getParameter("startDay");
+
+        Integer year = null;
+        ArrayList<Week> weeks = new ArrayList<>();
+        LocalDate startDay = null;
+        ArrayList<Date> daysOfWeek = new ArrayList<>();
+        LessonDBContext lessonDB = new LessonDBContext();
+        ArrayList<Lesson> LessonList = new ArrayList<>();
+
     } 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
-
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /** 
-     * Returns a short description of the servlet.
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account, ArrayList<Role> roles) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
