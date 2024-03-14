589f786142270a756a729ca7f2c3452b43ca19d2
Moodyzhub <phatuzumaki@gmail.com>
2024-03-14 22:34:51 +0700

    update data slot

--- ProjectFap/src/java/dal/TimeSlotDBContext.java
+++ ProjectFap/src/java/dal/TimeSlotDBContext.java
@@ -4,10 +4,60 @@
  */
 package dal;
 
+import entity.TimeSlot;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Fatvv
  */
-public class TimeSlotDBContext {
+public class TimeSlotDBContext extends DBContext<TimeSlot> {
+
+    @Override
+    public ArrayList<TimeSlot> list() {
+        
+        ArrayList<TimeSlot> slots = new ArrayList<>();
+        try {
+            
+            String sql = "SELECT tid,tname FROM TimeSlot";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while(rs.next())
+            {
+                TimeSlot slot = new TimeSlot();
+                slot.setTime_id(rs.getInt("tid"));
+                slot.setTime_name(rs.getString("tname"));
+                slots.add(slot);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TimeSlotDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return slots;
+    }
+
+    @Override
+    public void insert(TimeSlot entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(TimeSlot entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(TimeSlot entity) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public TimeSlot get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
