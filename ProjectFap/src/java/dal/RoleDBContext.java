86cc63730389a247e941219073944aae575e509e
Moodyzhub <phatuzumaki@gmail.com>
2024-03-15 23:09:16 +0700

    error 500
    đang fix để  getByUsernameAndUrl trả về roles thay vì null và  lại thành lỗi Error instantiating servlet class [controller.lecturer.TimeTableLecturerController]

--- ProjectFap/src/java/dal/RoleDBContext.java
+++ ProjectFap/src/java/dal/RoleDBContext.java
@@ -18,38 +18,55 @@ import java.util.logging.Logger;
  */
 public class RoleDBContext extends DBContext{
     
-    public ArrayList<Role> getByUsernameAndUrl(String username, String url){
-        ArrayList<Role> roles = new ArrayList();
-        try{
-            String sql = "SELECT r.roleid,r.rolename FROM Account a\n"
-                    + "	INNER JOIN Role_Account ra ON ra.username = a.username\n"
-                    + "	INNER JOIN [Role] r ON r.roleid = ra.roleid\n"
-                    + "	INNER JOIN [Role_Feature] rf ON rf.roleid = r.roleid\n"
-                    + "	INNER JOIN Feature f ON f.fid = rf.fid\n"
-                    + "WHERE \n"
-                    + "a.username = ? AND f.url = ?";
-            //Create a PreparedStatement object to execute parameterized SQL queries
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, username);
-            stm.setString(2, url);
-            //execute stm and store rs in object ResultSet
-            ResultSet rs = stm.executeQuery();
-            while(rs.next()){
-                Role r= new Role();
-                r.setRole_id(rs.getInt("roleid"));
-                r.setRole_name(rs.getString("rolename"));
-                roles.add(r);
-                
-            
-            }
-                 
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(RoleDBContext.class.getName()).log(Level.SEVERE, null, ex);
+//     public ArrayList<Role> getByUsernameAndUrl(String username, String url){
+//     ArrayList<Role> roles = new ArrayList();
+//     try{
+//         String sql = "SELECT r.roleid,r.rolename FROM Account a\n"
+//                 + "	INNER JOIN Role_Account ra ON ra.username = a.username\n"
+//                 + "	INNER JOIN [Role] r ON r.roleid = ra.roleid\n"
+//                 + "	INNER JOIN [Role_Feature] rf ON rf.roleid = r.roleid\n"
+//                 + "	INNER JOIN Feature f ON f.fid = rf.fid\n"
+//                 + "WHERE \n"
+//                 + "a.username = ? AND f.url = ?";
+//         PreparedStatement stm = connection.prepareStatement(sql);
+//         stm.setString(1, username);
+//         stm.setString(2, url);
+//         ResultSet rs = stm.executeQuery();
+//         while(rs.next()){
+//             Role r= new Role();
+//             r.setRole_id(rs.getInt("roleid"));
+//             r.setRole_name(rs.getString("rolename"));
+//             roles.add(r);
+//         }
+//     } catch (SQLException ex) {
+//         Logger.getLogger(RoleDBContext.class.getName()).log(Level.SEVERE, null, ex);
+//     }
+//     return roles; // return roles instead of null
+// }
+
+public ArrayList<Role> getByUsernameAndUrl(String username, String url){
+    ArrayList<Role> roles = new ArrayList();
+    try{
+        String sql = "SELECT r.roleid,r.rolename FROM Account a\n"
+                + "	INNER JOIN Role_Account ra ON ra.username = a.username\n"
+                + "	INNER JOIN [Role] r ON r.roleid = ra.roleid\n"
+                + "	INNER JOIN [Role_Feature] rf ON rf.roleid = r.roleid\n"
+                + "	INNER JOIN Feature f ON f.fid = rf.fid\n"
+                + "WHERE \n"
+                + "a.username = ? AND f.url = ?";
+        PreparedStatement stm = connection.prepareStatement(sql);
+        stm.setString(1, username);
+        stm.setString(2, url);
+        ResultSet rs = stm.executeQuery();
+        while(rs.next()){
+            Role r= new Role();
+            r.setRole_id(rs.getInt("roleid"));
+            r.setRole_name(rs.getString("rolename"));
+            roles.add(r);
         }
-        
-       
-        return null;
+    } catch (SQLException ex) {
+        Logger.getLogger(RoleDBContext.class.getName()).log(Level.SEVERE, null, ex);
     }
+    return roles; // return roles instead of null
 }
-    
+}
