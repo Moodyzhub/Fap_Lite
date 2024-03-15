253b8487376edf530b9087df1ecf905633fff9e6
Moodyzhub <phatuzumaki@gmail.com>
2024-03-16 04:10:42 +0700

    fix accountBD after fix entity
    sửa lại account sau khi sửa các entity khác , sửa lại chỉ cần 1 hàm getAccount thôi

--- ProjectFap/src/java/dal/AccountDBContext.java
+++ ProjectFap/src/java/dal/AccountDBContext.java
@@ -1,35 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
 import entity.Account;
+import entity.Lecturer;
 import entity.Student;
-import java.util.ArrayList;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Fatvv
- */
+
 public class AccountDBContext extends DBContext {
 
-    public Account getByUsernamePassword(String username, String password) {
+    
+    public Account getAccount(String username, String password) {
         try {
-            String sql = "SELECT username,password FROM Account\n"
-                    + " WHERE username = ? AND password = ?";
-            //Create a PreparedStatement object to execute parameterized SQL queries
+            Account account = new Account();
+            String sql = "SELECT a.username, a.password, s.id AS studentID, l.id AS lecturerID "
+           + "FROM Account a "
+           + "LEFT JOIN Student s ON a.student_id = s.id "
+           + "LEFT JOIN Lecturer l ON a.lecturer_id = l.id "
+           + "WHERE a.username = ? AND a.password = ?";
             PreparedStatement stm = connection.prepareStatement(sql);
             stm.setString(1, username);
             stm.setString(2, password);
             ResultSet rs = stm.executeQuery();
             if (rs.next()) {
-                Account account = new Account();
-                account.setUsername(username);
-                account.setPassword(password);
+                account.setUsername(rs.getString("Username"));
+                account.setPassword(rs.getString("Password"));
+                
+                if (rs.getString("studentID") != null) {
+                    Student student = new Student();
+                    student.setId(rs.getString("studentID"));
+                    account.setStudent(student);
+                }
+                
+                if(rs.getObject("lecturerID") != null){
+                    Lecturer lecturer = new Lecturer();
+                    lecturer.setId(rs.getInt("lecturerID"));
+                    account.setLecturer(lecturer);
+                }
+                
                 return account;
             }
         } catch (SQLException ex) {
@@ -37,51 +48,4 @@ public class AccountDBContext extends DBContext {
         }
         return null;
     }
-
-    public int getIdStudentByAcc(String username) {
-        try {
-            int idByAcc;
-            String sql = "SELECT s.sid \n"
-                    + "FROM Student s\n"
-                    + "JOIN Account a ON s.username = a.username\n"
-                    + "WHERE a.username = ?;";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, username);
-            ResultSet rs = stm.executeQuery();
-            if (rs.next()) {
-
-                return idByAcc = rs.getInt("sid");
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return 1;
-
-    }
-
-    public int getIdLecturerByAcc(String username) {
-        try {
-            int idByAcc;
-            String sql = "SELECT l.lecid "
-                    + "FROM Lecturer l "
-                    + "JOIN Account a ON l.username = a.username "
-                    + "WHERE a.username = ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, username);
-            ResultSet rs = stm.executeQuery();
-            if (rs.next()) {
-
-                return idByAcc = rs.getInt("lecid");
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(AccountDBContext.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return 1;
-
-    }
-
-    
-
 }
--- ProjectFap/src/java/entity/Feature.java
+++ ProjectFap/src/java/entity/Feature.java
@@ -9,5 +9,27 @@ package entity;
  * @author Fatvv
  */
 public class Feature {
+    private int id;
+    private String url;
+
+    public Feature() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
     
 }
